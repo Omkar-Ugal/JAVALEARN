034946195db481e75db6071969dafd96b2eeebc2
omkar-ugal <omkarugalmugle285@gmail.com>
2025-03-07 16:31:13 +0530

    sqrt

--- ArrayQuestion/twodNegativeArr.java
+++ ArrayQuestion/twodNegativeArr.java
@@ -1,6 +1,27 @@
 package ArrayQuestion;
 
 class Solution {
+    public int countNegativesBinarySerach(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans = 0;
+
+        int i =0;
+        while(i < n){
+            for(int j =0;j < m;j++){
+                if(grid[i][j] < 0){
+                    ans += n-j;
+                    ans += m-i-1;
+                    m--;
+                    break;
+                }
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
     public int countNegatives(int[][] grid) {
         int neg = 0;
         for(int i =0;i<grid.length;i++){
